92418c998b0ac790b0afb021ade91bdc97a58084
Shunya <wisteria.shunya@gmail.com>
2025-04-20 15:57:42 +0900

    Add : Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,104 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+  API_KEY: string,
+  DISCORD_WEBHOOK: string,
+
+  FORM_ID:string,
+  FORM_NAME_FIELD:string,
+  FORM_EMAIL_FIELD:string,
+  FORM_COMMENT_FIELD:string,
+  FORM_ENTRYID_FIELD:string,
+  COM_API_ENDPOINT:string
+}
+
+export interface ReqBody{
+  name: string,
+  mail: string,
+  comment: string,
+  entryId: string
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env:Env, ctx): Promise<Response> {
+    let res = {}
+
+    // コメント取得
+    if(request.method == "GET")
+    {
+      // クエリパラメータ取得
+      const reqUrl = new URL(request.url);
+      const params = reqUrl.searchParams;
+      const entryId = params.get("entry");
+
+      // GAS API call
+      const url = env.COM_API_ENDPOINT + "?entry=" + entryId;
+      const init = {method:"GET"};
+      const response = await fetch(url, init);
+      res = await response.json();
+    }
+
+    // コメント登録
+    else if(request.method == "POST")
+    {
+      const reqBody:ReqBody = await request.json();
+
+      // google form送信データ
+      const submitParams = new FormData();
+      submitParams.append(env.FORM_NAME_FIELD, reqBody.name);
+      submitParams.append(env.FORM_EMAIL_FIELD, reqBody.mail);
+      submitParams.append(env.FORM_COMMENT_FIELD, reqBody.comment);
+      submitParams.append(env.FORM_ENTRYID_FIELD, reqBody.entryId);
+      const gurl = "https://docs.google.com/forms/d/e/" + env.FORM_ID + "/formResponse";
+      const ginit = {
+        body: submitParams,
+        method: "POST"
+      }
+      // GoogleForm送信
+      const gresponse = await fetch(gurl, ginit);
+      // GoogleForm送信失敗
+      if(gresponse.status != 200)
+      {
+        res = {status : "E", code : "9001", resData : null};
+        return new Response(JSON.stringify(res), {headers:{
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+          "Content-Type": "application/json; charset=utf-8"
+        }});
+      }
+
+      // discordメッセージ送信
+      const sendMsg = {content: "swisteria.com にコメントが登録されました。\nお名前：" + reqBody.name + "\nエントリID：" + reqBody.entryId + "\nコメント：" + reqBody.comment};
+
+      const init = {
+        body: JSON.stringify(sendMsg),
+        method: "POST",
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+        }
+      };
+      const url = env.DISCORD_WEBHOOK;
+
+      const response = await fetch(url, init);
+      if(response.status > 299)
+      {
+        res = {status : "E", code : "9001", resData : init};
+        return new Response(JSON.stringify(res), {headers:{
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+          "Content-Type": "application/json; charset=utf-8"
+        }});
+      }
+
+      res = {status : "S", code : "0000", resData : init};
+
+    }
+
+    return new Response(JSON.stringify(res), {headers:{
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Content-Type": "application/json; charset=utf-8"
+    }});
+
+    // return Response.json(res);
+
+  },
 } satisfies ExportedHandler<Env>;
